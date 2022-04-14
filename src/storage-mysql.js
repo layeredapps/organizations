f5bf21d3020c0c4d108ae1c6f0bd3085f6a4eb4e
beninsydney <benodev@protonmail.com>
2022-04-14 13:33:27 +1000

    Consolidate schema

--- src/storage-mysql.js
+++ src/storage-mysql.js
@@ -1,4 +1,4 @@
-const { Sequelize, Model, DataTypes } = require('sequelize')
+const { Sequelize } = require('sequelize')
 
 module.exports = async () => {
   const prefixedDatabase = process.env.ORGANIZATIONS_MYSQL_DATABASE || process.env.MYSQL_DATABASE
@@ -17,94 +17,5 @@ module.exports = async () => {
       idle: process.env.ORGANIZATIONS_IDLE_CONNECTION_LIMIT || process.env.IDLE_CONNECTION_LIMIT || 10000
     }
   })
-  class Invitation extends Model {}
-  Invitation.init({
-    invitationid: {
-      type: DataTypes.STRING(21),
-      primaryKey: true,
-      defaultValue: () => {
-        const idValue = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
-        return 'invt_' + idValue.substring(0, 16)
-      }
-    },
-    organizationid: DataTypes.STRING(21),
-    accountid: DataTypes.STRING(21),
-    object: {
-      type: DataTypes.VIRTUAL,
-      get () {
-        return 'invitation'
-      }
-    },
-    appid: {
-      type: DataTypes.STRING,
-      defaultValue: global.appid
-    },
-    secretCodeHash: DataTypes.STRING,
-    acceptedAt: DataTypes.DATE
-  }, {
-    sequelize,
-    modelName: 'invitation'
-  })
-
-  class Membership extends Model {}
-  Membership.init({
-    membershipid: {
-      type: DataTypes.STRING(21),
-      primaryKey: true,
-      defaultValue: () => {
-        const idValue = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
-        return 'mmbr_' + idValue.substring(0, 16)
-      }
-    },
-    object: {
-      type: DataTypes.VIRTUAL,
-      get () {
-        return 'membership'
-      }
-    },
-    accountid: DataTypes.STRING(21),
-    organizationid: DataTypes.STRING(21),
-    invitationid: DataTypes.STRING(21),
-    profileid: DataTypes.STRING(21)
-  }, {
-    sequelize,
-    modelName: 'membership'
-  })
-
-  class Organization extends Model {}
-  Organization.init({
-    organizationid: {
-      type: DataTypes.STRING(21),
-      primaryKey: true,
-      defaultValue: () => {
-        const idValue = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
-        return 'orgn_' + idValue.substring(0, 16)
-      }
-    },
-    object: {
-      type: DataTypes.VIRTUAL,
-      get () {
-        return 'organization'
-      }
-    },
-    ownerid: DataTypes.STRING(21),
-    name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'organization'
-  })
-
-  await sequelize.sync()
-  return {
-    sequelize,
-    flush: async () => {
-      await Invitation.destroy({ where: {} })
-      await Membership.destroy({ where: {} })
-      await Organization.destroy({ where: {} })
-    },
-    Invitation,
-    Membership,
-    Organization
-  }
+  return sequelize
 }
--- src/storage.js
+++ src/storage.js
@@ -1,45 +1,168 @@
+const { Model, DataTypes } = require('sequelize')
 const metrics = require('@layeredapps/dashboard/src/metrics.js')
 
 module.exports = async () => {
-  let storage
+  let storage, dateType
   const prefixedStorage = process.env.ORGANIZATIONS_STORAGE || process.env.STORAGE
   switch (prefixedStorage) {
     case 'postgresql':
     case 'postgres':
       storage = require('./storage-postgresql.js')
+      dateType = DataTypes.DATE
       break
     case 'mariadb':
       storage = require('./storage-mariadb.js')
+      dateType = DataTypes.DATE(6)
       break
     case 'mysql':
       storage = require('./storage-mysql.js')
+      dateType = DataTypes.DATE(6)
       break
     case 'db2':
       storage = require('./storage-db2.js')
+      dateType = DataTypes.DATE
       break
     case 'mssql':
       storage = require('./storage-mssql.js')
+      dateType = DataTypes.DATE
       break
     case 'sqlite':
     default:
       storage = require('./storage-sqlite.js')
+      dateType = DataTypes.DATE
       break
   }
-  const container = await storage()
-  container.Organization.afterCreate(organizationsCreated)
-  container.Membership.afterCreate(membershipsCreated)
-  container.Invitation.afterCreate(invitationsCreated)
-  return container
-}
+  const sequelize = await storage()
+  class Invitation extends Model {}
+  Invitation.init({
+    invitationid: {
+      type: DataTypes.STRING(64),
+      primaryKey: true,
+      defaultValue: () => {
+        const idValue = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
+        return 'invt_' + idValue.substring(0, 16)
+      }
+    },
+    organizationid: DataTypes.STRING(64),
+    accountid: DataTypes.STRING(64),
+    object: {
+      type: DataTypes.VIRTUAL,
+      get () {
+        return 'invitation'
+      }
+    },
+    appid: {
+      type: DataTypes.STRING,
+      defaultValue: global.appid
+    },
+    secretCodeHash: DataTypes.STRING,
+    acceptedAt: dateType,
+    // 'createdAt' is specified for each model because mysql/mariadb truncate
+    // the ms and this makes the return order unpredictable and throws off the
+    // test suites expecting the write order to match the return order
+    createdAt: {
+      type: dateType,
+      allowNull: true,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'invitation'
+  })
 
-async function organizationsCreated () {
-  await metrics.aggregate('organizations-created', new Date())
-}
+  class Membership extends Model {}
+  Membership.init({
+    membershipid: {
+      type: DataTypes.STRING(64),
+      primaryKey: true,
+      defaultValue: () => {
+        const idValue = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
+        return 'mmbr_' + idValue.substring(0, 16)
+      }
+    },
+    object: {
+      type: DataTypes.VIRTUAL,
+      get () {
+        return 'membership'
+      }
+    },
+    appid: {
+      type: DataTypes.STRING,
+      defaultValue: global.appid
+    },
+    accountid: DataTypes.STRING(64),
+    organizationid: DataTypes.STRING(64),
+    invitationid: DataTypes.STRING(64),
+    profileid: DataTypes.STRING(64),
+    // 'createdAt' is specified for each model because mysql/mariadb truncate
+    // the ms and this makes the return order unpredictable and throws off the
+    // test suites expecting the write order to match the return order
+    createdAt: {
+      type: dateType,
+      allowNull: true,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'membership'
+  })
 
-async function membershipsCreated () {
-  await metrics.aggregate('memberships-created', new Date())
-}
-
-async function invitationsCreated () {
-  await metrics.aggregate('invitations-created', new Date())
+  class Organization extends Model {}
+  Organization.init({
+    organizationid: {
+      type: DataTypes.STRING(64),
+      primaryKey: true,
+      defaultValue: () => {
+        const idValue = Math.random().toString(16).substring(2) + Math.random().toString(16).substring(2)
+        return 'orgn_' + idValue.substring(0, 16)
+      }
+    },
+    object: {
+      type: DataTypes.VIRTUAL,
+      get () {
+        return 'organization'
+      }
+    },
+    appid: {
+      type: DataTypes.STRING,
+      defaultValue: global.appid
+    },
+    ownerid: DataTypes.STRING(64),
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    // 'createdAt' is specified for each model because mysql/mariadb truncate
+    // the ms and this makes the return order unpredictable and throws off the
+    // test suites expecting the write order to match the return order
+    createdAt: {
+      type: dateType,
+      allowNull: true,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'organization'
+  })
+  await sequelize.sync({ alter: true, force: true })
+  Organization.afterCreate(async () => {
+    await metrics.aggregate('organizations-created', new Date())
+  })
+  Membership.afterCreate(async () => {
+    await metrics.aggregate('memberships-created', new Date())
+  })
+  Invitation.afterCreate(async () => {
+    await metrics.aggregate('invitations-created', new Date())
+  })
+  return {
+    sequelize,
+    flush: async () => {
+      if (process.env.NODE_ENV === 'testing') {
+        await Organization.destroy({ where: {} })
+        await Membership.destroy({ where: {} })
+        await Invitation.destroy({ where: {} })
+      }
+    },
+    Organization,
+    Membership,
+    Invitation
+  }
 }
